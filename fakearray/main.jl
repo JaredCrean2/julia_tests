6bbfc59e5feda4290897cbe1b8dc11a281a50ed2
Jared Crean <jcrean01@gmail.com>
2015-12-18 21:13:17 -0500

    another implimentation, results look good

--- fakearray/main.jl
+++ fakearray/main.jl
@@ -2,29 +2,62 @@ using ArrayViews
 import ArrayViews.unsafe_view
 import Base: length, size, getindex, setindex!
 
-type FakeArray{T, N} <: AbstractArray{T,N}
-  a::Array{T, N}  # in reality, a would be a functor
+include("funcs.jl")
+
+type FakeArray{T, N, Tfunc} <: AbstractArray{T,N}
+  a::Array{T, N}
+  func::Tfunc
   buf::Array{T, 1} # buffer to hold output (temporarily)
 end
 
 
 
   
-function FakeArray(a::Array)
+function FakeArray(a::Array, func)
 
   T = eltype(a)
   N = length(size(a))
 
-  return FakeArray{T, N}(a, zeros(T, size(a, 1)))
+  return FakeArray{T, N, typeof(func)}(a, func, zeros(T, size(a, 1)))
 end
+
 
 function getindex(arr::FakeArray, i::Integer)
-  return arr.a[i]
+  idx = ind2sub(arr.a, i)
+  q_view = unsafe_view(arr.a, :, idx[2], idx[3])
+  arr.func(q_view, arr.buf)
+ 
+  return arr.buf[idx[1]]
 end
 
-function setindex(arr::FakeArray, i::Integer, v)
-  return arr.a[i] = v
+#=
+function getindex(arr::FakeArray, i::Integer, j::Integer)
+  return arr.a[i, j]
 end
+=#
+function getindex(arr::FakeArray, i::Integer, j::Integer, k::Integer)
+  q_view = unsafe_view(arr.a, :, j, k)
+  arr.funct(q_view, arr.buf) 
+  return arr.buf[i]
+end
+
+
+#=
+function setindex(arr::FakeArray, i::Integer, v)
+  idx = ind2sub(arr.a, i)
+  q_view = unsafe_view(arr.a, :, idx[2], idx[3])
+  arr.func(q_view, arr.buf)
+  return arr.buf[idx[1]]= v
+end
+#=
+function setindex(arr::FakeArray, i::Integer, j::Integer, v)
+  return arr.a[i, j] = v
+end
+=#
+function setindex(arr::FakeArray, i::Integer, j::Integer, k::Integer, v)
+  return arr.a[i, j, k] = v
+end
+=#
 
 
 function length(arr::FakeArray)
@@ -40,7 +73,7 @@ function printArr(arr::FakeArray)
     println("element $i = ", arr[i])
   end
 end
-
+#=
 immutable FakeArrayView{T, N} <: AbstractArray{T, N}
   a::Array{T,N}  # in reality, a would be a functor
   dims::NTuple{N, Int}
@@ -53,7 +86,7 @@ end
 function length(arr::FakeArrayView)
   return size(arr)[1]
 end
-
+=#
 # define our own ArrayViews type to go along with FakeArray
 # here we only support the case where we have a large array and
 # we are interested in taking a view of the first dimension.
@@ -63,31 +96,6 @@ end
 # We need the creation of a view to be nearly a no-op, ie. not copying or calculating anything
 # this is important because values are sometimes passed to functions but not
 # used, and we don't want there to be a performance penalty associated with that
-
-#=
-immutable FakeArrayUnsafeView{T, N} <: AbstractArray{T, N}
-  a::FakeArray{T, N}
-#  shp::NTuple{N, Int}
-end
-
-function unsafe_view{T, N}(arr::FakeArray{T, N}, arg1::Colon, i::Integer, j::Integer)
-  # calculate the quantity here, put it in the buffer
-  for k=1:size(arr, 1)
-    arr.buf[k] = arr.a[k, i, j]
-  end
-#  arr.buf[:] = arr.a[:, i, j]
-  return FakeArrayUnsafeView{T, N}(arr)  # the 1 is arbitary, unused
-end
-
-function unsafe_view{T, N}(arr::FakeArray{T, N}, arg1::Colon, i::Integer)
-  # calculate the quantity here, put it in the buffer
-  for k=1:size(arr, 1)
-    arr.buf[k] = arr.a[k, i]
-  end
-
-  return FakeArrayUnsafeView{T, N}(arr)  # the 1 is arbitary, unused
-end
-=#
 
 if false
 function unsafe_view{T, N}(arr::FakeArray{T, N}, arg1::Colon, i::Integer)
@@ -109,7 +117,7 @@ function unsafe_view{T, N}(arr::FakeArray{T, N}, arg1::Colon, i::Integer)
   return unsafe_view(arr.a, arg1, i)  # the 1 is arbitary, unused
 end
 
-else
+elseif false
 function unsafe_view{T, N}(arr::FakeArray{T, N}, arg1::Colon, i::Integer)
   tpl = (i, 1)
   av = FakeArrayView{T, N}(arr.a, tpl)  # the 1 is arbitrary, unused
@@ -121,29 +129,16 @@ function getindex(arr::FakeArrayView, i::Integer)
   return arr.a[i, arr.dims[1]]
 end
 
+else
+function unsafe_view(arr::FakeArray, arg1::Colon, i::Integer, j::Integer)
+  q_view = unsafe_view(arr.a, :, i, j)
+  arr.func(q_view, arr.buf)
+  return arr.buf
+end
+
 end
 
 
-
-#=
-function getindex(arr_view::FakeArrayUnsafeView, i::Integer)
-  return arr_view.a.buf[i]
-end
-
-function length(arr_view::FakeArrayUnsafeView)
-   return length(arr_view.a.buf)
-end
-=#
-
-#=
-function getindex(arr::FakeArray, args...)
-  return getindex(arr.a, args)
-end
-
-function setindex(arr::FakeArray, args...)
-  return setindex!(arr.a, args)
-end
-=#
 
 function sum_inner{T}(arr::AbstractArray{T})
   val = zero(T)
@@ -166,17 +161,46 @@ end
 
 
 # sum over dimension
-function sum3{T}(arr::AbstractArray{T})
+function sum3{T}(arr::FakeArray{T})
 
   val = zero(T)
-  for i=1:size(arr, 2)
-    arr_view = unsafe_view(arr, :, i)
-    val += sum_inner(arr_view)
+  for i=1:size(arr, 3)
+    for j=1:size(arr, 2)
+      arr_view = unsafe_view(arr, :, j, i)
+      val += sum_inner(arr_view)
+    end
   end
 
   return val
 end
 
+function sum3{T}(arr::Array{T}, F_arr::Array{T})
+# perform sum      
+  val = zero(T)
+  for i=1:size(arr, 3)
+    for j=1:size(arr, 2)
+      arr_view = unsafe_view(F_arr, :, j, i)
+      val += sum_inner(arr_view)
+    end
+  end
+
+  return val
+end
+
+
+function getFlux(arr::Array, F_arr::Array)
+# calculate Euler flux
+  for i=1:size(arr, 3)
+    for j=1:size(arr, 2)
+      f_view = unsafe_view(F_arr, :, j, i)
+      q_view = unsafe_view(arr, :, j, i)
+      calcEulerFlux(q_view, f_view)
+    end
+  end
+
+  return nothing
+end
+#=
 function sum4{T}(arr::AbstractArray{T})
 
   val = zero(T)
@@ -187,30 +211,48 @@ function sum4{T}(arr::AbstractArray{T})
 
   return val
 end
+=#
 
-a = rand(3, 1000000)
+#####################################################################
+n = 5000000
+a = zeros(4, 3, n)
+for i=1:n
+  for j=1:3
+    a[1] = 1.0
+    a[2] = 2.0
+    a[3] = 3.0
+    a[4] = 7.0
+  end
+end
+F_arr = zeros(size(a))
+
 println("sizeof(a) = ", sizeof(a)/1024, "KiB")
-obj = FakeArray(a)
+obj = FakeArray(a, FluxType())
 #printArr(obj)
 
 println("-----Warming Up-----")
+@time getFlux(a, F_arr)
 @time sum2(obj)
 @time sum2(a)
 @time sum3(obj)
-@time sum3(a)
-@time sum4(a)
+@time sum3(a, F_arr)
+#@time sum4(a)
 
 println("-----Direct Sum Final Results-----")
 @time sum2(obj)
 println("FakeArray @time printed above")
-@time sum2(a)
+@time getFlux(a, F_arr)
+println("  finished getting flux")
+@time sum2(F_arr)
 println("Array @time printed above")
 
 println("\n-----Dimensional Sum Final Results-----")
 @time sum3(obj)
 println("FakeArray @time printed above")
-@time sum3(a)
+@time getFlux(a, F_arr)
+println("finished getting flux")
+@time sum3(a, F_arr)
 println("Array @time printed above")
-@time sum4(a)
-println("view Array @time printed above")
+#@time sum4(a)
+#println("view Array @time printed above")
 
